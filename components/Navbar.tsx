5022169a38be303886fe58fc1e187a49371a6200
nguyenduythuan <thuan@cashd.com.au>
2023-08-16 18:14:09 +0700

    fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = ({ isPro }: NavbarProps) => {
     <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
         {/* <Menu className="block md:hidden" /> */}
-        <Mobilesidebar />
+        <Mobilesidebar isPro={isPro} />
         <Link href="/">
           <h1
             className={cn(
--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -2,14 +2,14 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
 import Sidebar from "./sidebar";
 
-const MobileSidebar = () => {
+const MobileSidebar = ({isPro} : {isPro: boolean}) => {
   return (
     <Sheet>
       <SheetTrigger className="md:hidden pr-4">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-        <Sidebar />
+        <Sidebar isPro={isPro}/>
       </SheetContent>
     </Sheet>
   );
